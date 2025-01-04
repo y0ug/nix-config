80219dc0670d423a976d143911916ee9ab11550f
Hugo Caron <hca443@gmail.com>
2025-01-04 21:34:50 +0100

    fix bug

--- darwin/configuration.nix
+++ darwin/configuration.nix
@@ -18,33 +18,6 @@
   # $ darwin-rebuild changelog
   system.stateVersion = 5;
 
-
-  # System-level packages
-  environment.systemPackages = with pkgs; [
-    vim
-    git
-  ];
-
-
-  # System settings
-  system = {
-    # For nix-darwin
-    stateVersion = 5;
-
-    # Set Git commit hash for darwin-version.
-    # configurationRevision = self.rev or self.dirtyRev or null;
-  };
-
-  # Keyboard
-  system.keyboard = {
-    enableKeyMapping = true;
-    remapCapsLockToEscape = true;
-  };
-
-  # Enable nix-darwin features
-  services.nix-daemon.enable = true;
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
-
   # Create /etc/zshrc that loads the nix-darwin environment
   programs.zsh.enable = true;
 
