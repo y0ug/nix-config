12f25b78fcf508be7ec837343b227e4a093a87ab
Hugo Caron <hca443@gmail.com>
2025-01-04 21:33:51 +0100

    fix bug

--- darwin/configuration.nix
+++ darwin/configuration.nix
@@ -1,12 +1,23 @@
-{ config, pkgs, lib, ... }: {
-  # Basic system configuration
-  users.users.rick = {
-    name = lib.user.name;
-    home = "/Users/${lib.user.name}";
-  };
+{ self, config, pkgs, lib, ... }: {
+  # List packages installed in system profile. To search by name, run:
+  # $ nix-env -qaP | grep wget
+  environment.systemPackages =
+    [ pkgs.vim
+    ];
 
-  # The platform the configuration will be used on.
-  nixpkgs.hostPlatform = "aarch64-darwin";
+  # Necessary for using flakes on this system.
+  nix.settings.experimental-features = "nix-command flakes";
+
+  # Enable alternative shell support in nix-darwin.
+  # programs.fish.enable = true;
+
+  # Set Git commit hash for darwin-version.
+  system.configurationRevision = self.rev or self.dirtyRev or null;
+
+  # Used for backwards compatibility, please read the changelog before changing.
+  # $ darwin-rebuild changelog
+  system.stateVersion = 5;
+
 
   # System-level packages
   environment.systemPackages = with pkgs; [
