396dc7c0c091119dffc862989ceffc1729f66483
Hugo Caron <hca443@gmail.com>
2025-01-06 15:56:20 +0100

    todo

--- home.nix
+++ home.nix
@@ -155,6 +155,8 @@ in
         use_fancy_tab_bar = false,
         enable_tab_bar = true,
         scrollback_lines = 10000,
+front_end = "WebGpu",
+webgpu_power_preference = "HighPerformance",
       }
       '';
   };
