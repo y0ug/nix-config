1a5a8d4975714f16bcde5e0b4bb2545b4d2b2d0e
Hugo Caron <hca443@gmail.com>
2025-01-06 14:02:41 +0100

    todo

--- darwin/configuration.nix
+++ darwin/configuration.nix
@@ -8,12 +8,6 @@
   # Necessary for using flakes on this system.
   nix.settings.experimental-features = "nix-command flakes";
 
-  # Enable alternative shell support in nix-darwin.
-  # programs.fish.enable = true;
-
-  # Set Git commit hash for darwin-version.
-  # system.configurationRevision = self.rev or self.dirtyRev or null;
-
   # Used for backwards compatibility, please read the changelog before changing.
   # $ darwin-rebuild changelog
   system.stateVersion = 5;
@@ -21,4 +15,30 @@
   # Create /etc/zshrc that loads the nix-darwin environment
   programs.zsh.enable = true;
 
+  fonts.fontDir.enable = true;
+
+  system = {
+    defaults = {
+      NSGlobalDomain = {
+        AppleShowAllExtensions = true;
+        InitialKeyRepeat = 15;
+        KeyRepeat = 2;
+      };
+      dock = {
+        autohide = true;
+        show-recents = false;
+        mru-spaces = false;
+      };
+      finder = {
+        AppleShowAllExtensions = true;
+        _FXShowPosixPathInTitle = true;
+      };
+    };
+    stateVersion = 4;
+  };
+
+  system.keyboard = {
+    enableKeyMapping = true;
+    remapCapsLockToEscape = true;
+  };
 }
