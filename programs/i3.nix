ad620824aa2a3ff440fbe069682a165d15f80ef3
Hugo Caron <hca443@gmail.com>
2025-01-31 13:41:52 +0100

    fix: escape characters in i3 config keybindings
    - Adjusted syntax for proper escape sequences in template strings

--- programs/i3.nix
+++ programs/i3.nix
@@ -14,8 +14,8 @@
     config = {
       modifier = "Mod4";
       keybindings = {
-        "${modifier}+Return" = "exec ${pkgs.wezterm}/bin/wezterm";
-        "${modifier}+d" = "exec ${pkgs.rofi}/bin/rofi -show drun";
+        "\${modifier}+Return" = "exec ${pkgs.wezterm}/bin/wezterm";
+        "\${modifier}+d" = "exec ${pkgs.rofi}/bin/rofi -show drun";
       };
     };
   };
