a370fd2b4f9453e0aafff0f167e151b8e4e5c8ef
Hugo Caron <hca443@gmail.com>
2025-01-31 14:17:40 +0100

    fix homemanager backupFileExtension

--- home/nixos-vm.nix
+++ home/nixos-vm.nix
@@ -1,20 +1,11 @@
-{ config, pkgs, ... }:
-{
-  imports = [
-    ./linux.nix
-    ../programs/i3.nix
-    ../programs/wezterm.nix
-  ];
+{ config, pkgs, ... }: {
+  imports = [ ./linux.nix ../programs/i3.nix ../programs/wezterm.nix ];
 
   home.username = "rick";
   home.homeDirectory = "/home/rick";
 
-  home.packages = with pkgs; [
-    firefox
-  ];
+  home.packages = with pkgs; [ firefox ];
 
-
-  xsession.windowManager.i3.config.startup = [
-    { command = "exec ${pkgs.nitrogen}/bin/nitrogen --restore"; }
-  ];
+  xsession.windowManager.i3.config.startup =
+    [{ command = "exec ${pkgs.nitrogen}/bin/nitrogen --restore"; }];
 }
--- hosts/nixos-vm/configuration.nix
+++ hosts/nixos-vm/configuration.nix
@@ -1,9 +1,8 @@
-{ config, pkgs, ... }:
-{
+{ config, pkgs, ... }: {
   imports = [ ./hardware-configuration.nix ];
 
   # Use the systemd-boot EFI boot loader.
-  boot.loader.systemd-boot.enable = true; 
+  boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
   networking.hostName = "nixos-vm";
@@ -11,20 +10,13 @@
   time.timeZone = "Europe/Paris";
 
   # System-level GUI packages
-  environment.systemPackages = with pkgs; [
-    xorg.xkill
-    xorg.xrandr
-    i3
-    lightdm
-  ];
+  environment.systemPackages = with pkgs; [ xorg.xkill xorg.xrandr i3 lightdm ];
 
   # X11 configuration
   services.xserver = {
     enable = true;
     # displayManager.lightdm.enable = true;
-    displayManager = {
-        defaultSession = "none+i3";
-    };
+    displayManager = { defaultSession = "none+i3"; };
     windowManager.i3.enable = true;
   };
 
@@ -33,22 +25,22 @@
 
   users.users.rick = {
     isNormalUser = true;
-    extraGroups = [ "wheel"];
-    packages = with pkgs; [
-      tree
-      neovim
-      chezmoi
+    extraGroups = [ "wheel" ];
+    packages = with pkgs; [ tree neovim chezmoi ];
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIILPlZ1DnETw4zdAVGB4uQL/MBj42qmPU3nzaR6g4SEe rick@mazenet.org"
     ];
-    openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIILPlZ1DnETw4zdAVGB4uQL/MBj42qmPU3nzaR6g4SEe rick@mazenet.org" ];
   };
+
+  home-manager.backupFileExtension = "backup-" + pkgs.lib.readFile
+    "${pkgs.runCommand "timestamp" { }
+    "echo -n `date '+%Y%m%d%H%M%S'` > $out"}";
 
   nix = {
     settings = {
-  experimental-features = [ "nix-command" "flakes" ];
-      substituters = [
-        "https://nix-community.cachix.org"
-        "https://cache.nixos.org/"
-      ];
+      experimental-features = [ "nix-command" "flakes" ];
+      substituters =
+        [ "https://nix-community.cachix.org" "https://cache.nixos.org/" ];
       trusted-public-keys = [
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
