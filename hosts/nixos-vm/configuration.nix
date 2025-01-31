a12fcd0e42f48c86fe82d5c1979e1dad8dd5fc51
Hugo Caron <hca443@gmail.com>
2025-01-31 21:49:03 +0100

    feat: enhance hyprland configuration
    - Enable UWSM and xwayland for hyprland setup
    - Provide recommendations for most users

--- hosts/nixos-vm/configuration.nix
+++ hosts/nixos-vm/configuration.nix
@@ -9,7 +9,12 @@
 
   time.timeZone = "Europe/Paris";
 
-  programs.hyprland.enable = true;
+
+  programs.hyprland = {
+    enable = true;
+    withUWSM = true; # recommended for most users
+    xwayland.enable = true; # Xwayland can be disabled.
+  };
 
   # System-level GUI packages
   environment.systemPackages = with pkgs; [ 
