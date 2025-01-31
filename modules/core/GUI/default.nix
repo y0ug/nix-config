4b14da5448b60fa1ac52a98f2bca687f3da859b1
Hugo Caron <hca443@gmail.com>
2025-02-01 00:28:18 +0100

    refactor: update import statement in default.nix
    - Comment out dynamic import logic
    - Add explicit imports for specific config files

--- modules/core/GUI/default.nix
+++ modules/core/GUI/default.nix
@@ -1,9 +1,16 @@
 { config, pkgs, ... }:
 
 {
-  imports = let
-    programFiles = builtins.attrNames (builtins.readDir ./.);
-    filteredPrograms =
-      builtins.filter (name: name != "default.nix") programFiles;
-  in map (name: ./. + "/${name}") filteredPrograms;
+  # imports = let
+  #   programFiles = builtins.attrNames (builtins.readDir ./.);
+  #   filteredPrograms =
+  #     builtins.filter (name: name != "default.nix") programFiles;
+  # in map (name: ./. + "/${name}") filteredPrograms;
+  imports = [
+    ./fonts.nix
+    ./program.nix
+    ./security.nix
+    ./services.nix
+    ./xserver.nix
+  ];
 }
