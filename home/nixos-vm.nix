90dec96296f57d5bd659bfbfc7ecf4cdfad2ae76
Hugo Caron <hca443@gmail.com>
2025-01-31 14:07:47 +0100

    refactor: update i3 and wezterm configuration
    - Remove unnecessary comments and streamline package list
    - Update i3 configuration setup path for better clarity

--- home/nixos-vm.nix
+++ home/nixos-vm.nix
@@ -10,13 +10,11 @@
   home.homeDirectory = "/home/rick";
 
   home.packages = with pkgs; [
-    # firefox
+    firefox
   ];
 
-  programs.i3.enable = true;
-  programs.wezterm.enable = true
 
-  programs.i3.config.startup = [
+  xsession.windowManager.i3.config.startup = [
     { command = "exec ${pkgs.nitrogen}/bin/nitrogen --restore"; }
   ];
 }
