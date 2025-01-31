2a8f24f1c1f37fb7ab3197ac6108fe0fd19d3827
Hugo Caron <hca443@gmail.com>
2025-01-31 21:50:26 +0100

    chore: comment out hyprland configuration
    - Commented out Hyprland and kitty configurations in hyprland.nix

--- programs/hyprland.nix
+++ programs/hyprland.nix
@@ -3,17 +3,17 @@
 {
   imports = [./wezterm.nix];
 
-  programs.kitty.enable = true;
-  wayland.windowManager.hyprland = {
-    # Whether to enable Hyprland wayland compositor
-    enable = true;
-    # The hyprland package to use
-    package = pkgs.hyprland;
-    # Whether to enable XWayland
-    xwayland.enable = true;
-
-    # Optional
-    # Whether to enable hyprland-session.target on hyprland startup
-    systemd.enable = true;
-  };
+  # programs.kitty.enable = true;
+  # wayland.windowManager.hyprland = {
+  #   # Whether to enable Hyprland wayland compositor
+  #   enable = true;
+  #   # The hyprland package to use
+  #   package = pkgs.hyprland;
+  #   # Whether to enable XWayland
+  #   xwayland.enable = true;
+  #
+  #   # Optional
+  #   # Whether to enable hyprland-session.target on hyprland startup
+  #   systemd.enable = true;
+  # };
 }
