be86bdaa1a6daf91f5348058e5fd697ad0a1ed23
Hugo Caron <hca443@gmail.com>
2025-02-01 17:14:27 +0100

    feat: remove unused gnome packages and services
    - Remove gnome-settings-daemon service
    - Exclude several default gnome applications and utilities
    - Keep core components and essential tools

--- modules/core/GUI/gnome.nix
+++ modules/core/GUI/gnome.nix
@@ -5,15 +5,56 @@
 
   # Enable CUPS to print documents.
   services.printing.enable = true;
-  
-  services.gnome.gnome-settings-daemon = {
-    enable = true;
-    extraConfig = ''
-      [org/gnome/settings-daemon/plugins/power]
-      sleep-inactive-ac-timeout=0
-      sleep-inactive-battery-timeout=0
-      sleep-inactive-ac-type='nothing'
-      sleep-inactive-battery-type='nothing'
-    '';
-  };
+
+  environment.gnome.excludePackages = with pkgs; [
+    orca
+    evince
+    # file-roller
+    geary
+    gnome-disk-utility
+    # seahorse
+    # sushi
+    # sysprof
+    #
+    # gnome-shell-extensions
+    #
+    # adwaita-icon-theme
+    # nixos-background-info
+    gnome-backgrounds
+    # gnome-bluetooth
+    # gnome-color-manager
+    # gnome-control-center
+    # gnome-shell-extensions
+    gnome-tour # GNOME Shell detects the .desktop file on first log-in.
+    gnome-user-docs
+    # glib # for gsettings program
+    # gnome-menus
+    # gtk3.out # for gtk-launch program
+    # xdg-user-dirs # Update user dirs as described in https://freedesktop.org/wiki/Software/xdg-user-dirs/
+    # xdg-user-dirs-gtk # Used to create the default bookmarks
+    #
+    baobab
+    epiphany
+    gnome-text-editor
+    gnome-calculator
+    gnome-calendar
+    gnome-characters
+    # gnome-clocks
+    gnome-console
+    gnome-contacts
+    gnome-font-viewer
+    gnome-logs
+    gnome-maps
+    gnome-music
+    # gnome-system-monitor
+    gnome-weather
+    # loupe
+    # nautilus
+    gnome-connections
+    simple-scan
+    snapshot
+    totem
+    yelp
+    gnome-software
+  ];
 }
