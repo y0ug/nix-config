c6f6d62d1388cafe22475183043b62db1d7a8358
Hugo Caron <hca443@gmail.com>
2025-05-01 14:12:41 +0200

    refactor: modularize system configuration
    - Move udev rules to dedicated module
    - Add flatpak support
    - Configure YubiKey and smartcard services
    - Improve GPG agent configuration

--- hosts/culixa/configuration.nix
+++ hosts/culixa/configuration.nix
@@ -17,6 +17,8 @@
     # ../../modules/core/GUI/gnome.nix
     ../../modules/core/GUI/hyprland.nix
     # ../../modules/pinned/devenv-1.5.1.nix
+    ../../modules/core/flatpak.nix
+    ../../modules/core/udev-rules.nix
 
   ];
   nix = {
@@ -171,7 +173,6 @@
     pinentry-curses
   ];
 
-  services.udev.packages = [ pkgs.via ];
   environment.localBinInPath = true;
 
   # allow non-nix executable
@@ -183,22 +184,29 @@
   programs.gnupg.agent = {
     enable = true;
     enableSSHSupport = true;
-    settings = {
-      ignore-cache-errors = true;
-    };
+    enableBrowserSocket = true;
+    pinentryProgram = pkgs.pinentry-curses;
+    settings = { };
   };
 
   # List services that you want to enable:
 
-  # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
-
-  services.sunshine = {
-    enable = true;
-    autoStart = true;
-    capSysAdmin = true;
-    openFirewall = true;
+  services = {
+    openssh.enable = true;
+    yubikey-agent.enable = true;
+    pcscd.enable = true; # smart card reader daemon
+    udev.packages = [
+      pkgs.yubikey-personalization
+      pkgs.via
+    ];
+    sunshine = {
+      enable = true;
+      autoStart = true;
+      capSysAdmin = true;
+      openFirewall = true;
+    };
   };
+  # Enable the OpenSSH daemon.
 
   networking.firewall = {
     enable = true;
@@ -221,12 +229,6 @@
     ];
   };
 
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ 22 ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. ItΓÇÿs perfectly fine and recommended to leave
@@ -245,97 +247,4 @@
     });
   '';
 
-  services.udev.extraRules = ''
-    # annerpro flash mode 04d9:8008
-    SUBSYSTEMS=="usb", ATTRS{idVendor}=="04d9", ATTRS{idProduct}=="8009", TAG+="uaccess"
-    # Atmel DFU
-    ### ATmega16U2
-    SUBSYSTEMS=="usb", ATTRS{idVendor}=="03eb", ATTRS{idProduct}=="2fef", TAG+="uaccess"
-    ### ATmega32U2
-    SUBSYSTEMS=="usb", ATTRS{idVendor}=="03eb", ATTRS{idProduct}=="2ff0", TAG+="uaccess"
-    ### ATmega16U4
-    SUBSYSTEMS=="usb", ATTRS{idVendor}=="03eb", ATTRS{idProduct}=="2ff3", TAG+="uaccess"
-    ### ATmega32U4
-    SUBSYSTEMS=="usb", ATTRS{idVendor}=="03eb", ATTRS{idProduct}=="2ff4", TAG+="uaccess"
-    ### AT90USB64
-    SUBSYSTEMS=="usb", ATTRS{idVendor}=="03eb", ATTRS{idProduct}=="2ff9", TAG+="uaccess"
-    ### AT90USB162
-    SUBSYSTEMS=="usb", ATTRS{idVendor}=="03eb", ATTRS{idProduct}=="2ffa", TAG+="uaccess"
-    ### AT90USB128
-    SUBSYSTEMS=="usb", ATTRS{idVendor}=="03eb", ATTRS{idProduct}=="2ffb", TAG+="uaccess"
-
-    # Input Club
-    SUBSYSTEMS=="usb", ATTRS{idVendor}=="1c11", ATTRS{idProduct}=="b007", TAG+="uaccess"
-
-    # STM32duino
-    SUBSYSTEMS=="usb", ATTRS{idVendor}=="1eaf", ATTRS{idProduct}=="0003", TAG+="uaccess"
-    # STM32 DFU
-    SUBSYSTEMS=="usb", ATTRS{idVendor}=="0483", ATTRS{idProduct}=="df11", TAG+="uaccess"
-
-    # BootloadHID
-    SUBSYSTEMS=="usb", ATTRS{idVendor}=="16c0", ATTRS{idProduct}=="05df", TAG+="uaccess"
-
-    # USBAspLoader
-    SUBSYSTEMS=="usb", ATTRS{idVendor}=="16c0", ATTRS{idProduct}=="05dc", TAG+="uaccess"
-
-    # USBtinyISP
-    SUBSYSTEMS=="usb", ATTRS{idVendor}=="1782", ATTRS{idProduct}=="0c9f", TAG+="uaccess"
-
-    # ModemManager should ignore the following devices
-    # Atmel SAM-BA (Massdrop)
-    SUBSYSTEMS=="usb", ATTRS{idVendor}=="03eb", ATTRS{idProduct}=="6124", TAG+="uaccess", ENV{ID_MM_DEVICE_IGNORE}="1"
-
-    # Caterina (Pro Micro)
-    ## pid.codes shared PID
-    ### Keyboardio Atreus 2 Bootloader
-    SUBSYSTEMS=="usb", ATTRS{idVendor}=="1209", ATTRS{idProduct}=="2302", TAG+="uaccess", ENV{ID_MM_DEVICE_IGNORE}="1"
-    ## Spark Fun Electronics
-    ### Pro Micro 3V3/8MHz
-    SUBSYSTEMS=="usb", ATTRS{idVendor}=="1b4f", ATTRS{idProduct}=="9203", TAG+="uaccess", ENV{ID_MM_DEVICE_IGNORE}="1"
-    ### Pro Micro 5V/16MHz
-    SUBSYSTEMS=="usb", ATTRS{idVendor}=="1b4f", ATTRS{idProduct}=="9205", TAG+="uaccess", ENV{ID_MM_DEVICE_IGNORE}="1"
-    ### LilyPad 3V3/8MHz (and some Pro Micro clones)
-    SUBSYSTEMS=="usb", ATTRS{idVendor}=="1b4f", ATTRS{idProduct}=="9207", TAG+="uaccess", ENV{ID_MM_DEVICE_IGNORE}="1"
-    ## Pololu Electronics
-    ### A-Star 32U4
-    SUBSYSTEMS=="usb", ATTRS{idVendor}=="1ffb", ATTRS{idProduct}=="0101", TAG+="uaccess", ENV{ID_MM_DEVICE_IGNORE}="1"
-    ## Arduino SA
-    ### Leonardo
-    SUBSYSTEMS=="usb", ATTRS{idVendor}=="2341", ATTRS{idProduct}=="0036", TAG+="uaccess", ENV{ID_MM_DEVICE_IGNORE}="1"
-    ### Micro
-    SUBSYSTEMS=="usb", ATTRS{idVendor}=="2341", ATTRS{idProduct}=="0037", TAG+="uaccess", ENV{ID_MM_DEVICE_IGNORE}="1"
-    ## Adafruit Industries LLC
-    ### Feather 32U4
-    SUBSYSTEMS=="usb", ATTRS{idVendor}=="239a", ATTRS{idProduct}=="000c", TAG+="uaccess", ENV{ID_MM_DEVICE_IGNORE}="1"
-    ### ItsyBitsy 32U4 3V3/8MHz
-    SUBSYSTEMS=="usb", ATTRS{idVendor}=="239a", ATTRS{idProduct}=="000d", TAG+="uaccess", ENV{ID_MM_DEVICE_IGNORE}="1"
-    ### ItsyBitsy 32U4 5V/16MHz
-    SUBSYSTEMS=="usb", ATTRS{idVendor}=="239a", ATTRS{idProduct}=="000e", TAG+="uaccess", ENV{ID_MM_DEVICE_IGNORE}="1"
-    ## dog hunter AG
-    ### Leonardo
-    SUBSYSTEMS=="usb", ATTRS{idVendor}=="2a03", ATTRS{idProduct}=="0036", TAG+="uaccess", ENV{ID_MM_DEVICE_IGNORE}="1"
-    ### Micro
-    SUBSYSTEMS=="usb", ATTRS{idVendor}=="2a03", ATTRS{idProduct}=="0037", TAG+="uaccess", ENV{ID_MM_DEVICE_IGNORE}="1"
-
-    # hid_listen
-    KERNEL=="hidraw*", MODE="0660", GROUP="plugdev", TAG+="uaccess", TAG+="udev-acl"
-
-    # hid bootloaders
-    ## QMK HID
-    SUBSYSTEMS=="usb", ATTRS{idVendor}=="03eb", ATTRS{idProduct}=="2067", TAG+="uaccess"
-    ## PJRC's HalfKay
-    SUBSYSTEMS=="usb", ATTRS{idVendor}=="16c0", ATTRS{idProduct}=="0478", TAG+="uaccess"
-
-    # APM32 DFU
-    SUBSYSTEMS=="usb", ATTRS{idVendor}=="314b", ATTRS{idProduct}=="0106", TAG+="uaccess"
-
-    # GD32V DFU
-    SUBSYSTEMS=="usb", ATTRS{idVendor}=="28e9", ATTRS{idProduct}=="0189", TAG+="uaccess"
-
-    # WB32 DFU
-    SUBSYSTEMS=="usb", ATTRS{idVendor}=="342d", ATTRS{idProduct}=="dfa0", TAG+="uaccess"
-
-    # AT32 DFU
-    SUBSYSTEMS=="usb", ATTRS{idVendor}=="2e3c", ATTRS{idProduct}=="df11", TAG+="uaccess"
-  '';
 }
--- modules/home/git.nix
+++ modules/home/git.nix
@@ -1,4 +1,5 @@
-{ inputs, ... }: {
+{ inputs, ... }:
+{
   programs.git = {
     enable = true;
 
